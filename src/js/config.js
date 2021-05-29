e59f6346f2cf9900f884983f8e98c91c2d5d5505
JohnnyDonD <johnnydondras@live.com>
2021-05-29 10:56:08 +0200

    initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'ef801fea-9fa4-4778-8059-cd9d57ddc662';
+export const MODAL_CLOSE_SEC = 2.5;
